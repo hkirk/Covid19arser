c7b603dc2c0355e45c82e7131f18c6a4442012ba
Henrik Kirk <henrik@busywait.org>
2020-11-03 15:24:34 +0100

    Added CSV output

--- Program.cs
+++ Program.cs
@@ -16,16 +16,19 @@ namespace Covid19Data
       string line = "";
       var reader = new StreamReader("./Municipality_test_pos.csv");
       var builder = new StringBuilder();
+      var builderCSV = new StringBuilder();
       reader.ReadLine(); // Skip first
       while ((line = reader.ReadLine()) != null)
       {
         var data = line.Split(";");
         var population = float.Parse(data[4].Trim()) * 1000;
         builder.Append($"new Municipality({data[0].Trim()}, \"{data[1].Trim()}\", {population}),\n");
+        builderCSV.Append($"{data[0].Trim()},{data[1].Trim()},{population},\n");
 
       }
 
       File.WriteAllText("./output/Municipality.output", builder.ToString());
+      File.WriteAllText("./output/Municipality.csv", builderCSV.ToString());
 
       reader.Close();
     }
